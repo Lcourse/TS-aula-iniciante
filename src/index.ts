c83d590664648f491aac3deb6cb3eb6bd55388cc
Lcourse <101373417+Lcourse@users.noreply.github.com>
2022-09-30 20:32:40 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -126,3 +126,150 @@ function greeting(name: string, greet?: string) {
 
 greeting("Lcourse");
 greeting("Lucas", "Sir");
+
+// interfaces
+interface MathFunctionParams {
+  n1: number;
+  n2: number;
+}
+
+function sumNumbers(nums: MathFunctionParams) {
+  return nums.n1 + nums.n2;
+}
+
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+
+function multiplyNumbers(nums: MathFunctionParams) {
+  return nums.n1 * nums.n2;
+}
+
+const someNumbers: MathFunctionParams = {
+  n1: 5,
+  n2: 10,
+};
+
+console.log(multiplyNumbers(someNumbers));
+
+// narrowing
+// checagem tipos
+function doSomething(info: number | boolean) {
+  if (typeof info === "number") {
+    console.log(`o número é ${info}`);
+    return;
+  }
+  console.log("Não foi passado um número");
+}
+
+doSomething(5);
+doSomething(true);
+
+// generics
+function showArraysItems<T>(arr: T[]) {
+  arr.forEach((item) => {
+    console.log(`ITEM: ${item}`);
+  });
+}
+
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+
+showArraysItems(a1);
+showArraysItems(a2);
+
+// classes
+class User {
+  name;
+  role;
+  isApproved;
+
+  constructor(name: string, role: string, isApproved: boolean) {
+    this.name = name;
+    this.role = role;
+    this.isApproved = isApproved;
+  }
+
+  showUserName() {
+    console.log(`O nome do usuário é ${this.name}`);
+  }
+
+  showUserRole(canShow: boolean) {
+    if (canShow) {
+      console.log(`Idade do usuário é: ${this.role}`);
+      return;
+    }
+    console.log("Informação restrita");
+  }
+}
+
+const Lcourse = new User("Lcourse", "Admin", true);
+
+console.log(Lcourse);
+
+Lcourse.showUserName();
+
+Lcourse.showUserRole(false);
+
+// interfaces em classes
+interface IVehicle {
+  brand: string;
+  showBrand(): void;
+}
+
+class Car implements IVehicle {
+  brand;
+  wheels;
+
+  constructor(brand: string, wheels: number) {
+    this.brand = brand;
+    this.wheels = wheels;
+  }
+
+  showBrand(): void {
+    console.log(`A marca do carro é: ${this.brand}`);
+  }
+}
+
+const fusca = new Car("VW", 4);
+
+fusca.showBrand();
+
+// heranca
+class SuperCar extends Car {
+  engine;
+
+  constructor(Brand: string, wheels: number, engine: number) {
+    super(brand, wheels);
+    this.engine = engine;
+  }
+}
+
+const a4 = new SuperCar("Audi", 4, 2.0);
+
+console.log(a4);
+
+a4.showBrand();
+
+// decorators
+
+// constructor decorator
+function BaseParameters() {
+  return function <T extends { new (...args: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+      id = Math.random();
+      createAt = new Date();
+    };
+  };
+}
+
+@BaseParameters()
+class Person {
+  name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const sam = new Person("Sam");
+
+console.log(sam);
