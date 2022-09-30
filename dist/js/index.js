5b35cba757be1c8aeb87b130d92278ac44aa9f28
Lcourse <101373417+Lcourse@users.noreply.github.com>
2022-09-30 20:31:53 -0300

    Update index.js

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,53 +1,44 @@
 "use strict";
-// string, boolean, number, ...
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 let x = 10;
 x = 12;
 console.log(x);
-// interferencia x annotation
 let y = 12;
-// y = "teste";
 let z = 12;
-// tipos básicos
 let firstName = "Lucas";
 let age = 28;
 const isAdmin = true;
-// string != string
 console.log(typeof firstName);
 firstName = "Lcourse";
 console.log(firstName);
-// object
 const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length);
-// console.log(myNumbers.parseFloat());
 console.log(firstName.toUpperCase());
 myNumbers.push(5);
 console.log(myNumbers);
-// tuplas -> tuple
 let myTuple;
 myTuple = [5, "teste", ["a", "b"]];
-// myTuple = [true, false, true];
-// object literals -> {prop: value}
 const user = {
     Name: "Lucas",
     age: 28,
 };
 console.log(user);
 console.log(user.name);
-// user.job = "Programador"
-//any
 let a = 0;
 a = "teste";
 a = true;
 a = [];
-// union type
 let id = "10";
 id = 200;
 const userId = 10;
 const productId = "00001";
 const shirId = 123;
-// enum
-// tamanho de roupas (size: Médio, size: Pequeno)
 var size;
 (function (size) {
     size["P"] = "Pequeno";
@@ -59,16 +50,12 @@ const camisa = {
     size: size.G,
 };
 console.log(camisa);
-// literal types
 let teste;
-// teste = "outrovalor";
 teste = "autenticado";
-// funcoes
 function sum(a, b) {
     return a + b;
 }
 console.log(sum(12, 12));
-// console.log(sum("12", true))
 function sayHelloTo(name) {
     return `hello ${name}`;
 }
@@ -86,3 +73,99 @@ function greeting(name, greet) {
 }
 greeting("Lcourse");
 greeting("Lucas", "Sir");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+const someNumbers = {
+    n1: 5,
+    n2: 10,
+};
+console.log(multiplyNumbers(someNumbers));
+function doSomething(info) {
+    if (typeof info === "number") {
+        console.log(`o número é ${info}`);
+        return;
+    }
+    console.log("Não foi passado um número");
+}
+doSomething(5);
+doSomething(true);
+function showArraysItems(arr) {
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+showArraysItems(a1);
+showArraysItems(a2);
+class User {
+    name;
+    role;
+    isApproved;
+    constructor(name, role, isApproved) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+    showUserName() {
+        console.log(`O nome do usuário é ${this.name}`);
+    }
+    showUserRole(canShow) {
+        if (canShow) {
+            console.log(`Idade do usuário é: ${this.role}`);
+            return;
+        }
+        console.log("Informação restrita");
+    }
+}
+const Lcourse = new User("Lcourse", "Admin", true);
+console.log(Lcourse);
+Lcourse.showUserName();
+Lcourse.showUserRole(false);
+class Car {
+    brand;
+    wheels;
+    constructor(brand, wheels) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+    showBrand() {
+        console.log(`A marca do carro é: ${this.brand}`);
+    }
+}
+const fusca = new Car("VW", 4);
+fusca.showBrand();
+class SuperCar extends Car {
+    engine;
+    constructor(Brand, wheels, engine) {
+        super(brand, wheels);
+        this.engine = engine;
+    }
+}
+const a4 = new SuperCar("Audi", 4, 2.0);
+console.log(a4);
+a4.showBrand();
+function BaseParameters() {
+    return function (constructor) {
+        return class extends constructor {
+            id = Math.random();
+            createAt = new Date();
+        };
+    };
+}
+let Person = class Person {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+};
+Person = __decorate([
+    BaseParameters()
+], Person);
+const sam = new Person("Sam");
+console.log(sam);
